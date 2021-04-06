4c56bd139bacc85a7b4c1a7edece6ea16ae98720
joy <joy@joyui-MacBook.local>
2021-04-07 07:53:28 +0900

    add googleGuice

---
+++ src/test/java/com/uangel/training/GoogleGuiceExample.java
@@ -0,0 +1,54 @@
+import com.google.inject.AbstractModule;
+import com.google.inject.Guice;
+import com.google.inject.Inject;
+import com.google.inject.Injector;
+
+import javax.inject.Singleton;
+
+public class GoogleGuiceExample {
+    public static void main(String[] args) {
+        Injector injector = Guice.createInjector(new AppInjector());
+        MyController app = injector.getInstance(MyController.class); // at this moment the data was injected
+        System.out.println(app.getDatabaseData());
+    }
+}
+
+class MyController {
+    @Inject //inject directly the property
+    private Database db;
+    //  @Inject //injcting by param with setter methode. Or you can inject the data by contructor injection
+//  public void readDatabaseData(Database db) {
+//    this.db = db;
+//  }
+    public String getDatabaseData() {
+        return db.getData();
+    }
+}
+
+// configure which database should be used!
+class AppInjector extends AbstractModule {
+    @Override
+    protected void configure() {
+        bind(Database.class).to(InMemoryDatabase.class);
+    }
+}
+
+interface Database {
+    public String getData();
+}
+
+@Singleton
+class InMemoryDatabase implements Database {
+    @Override
+    public String getData() {
+        return "data from InMemoryDatabase";
+    }
+}
+
+@Singleton
+class MySqlDatabase implements Database {
+    @Override
+    public String getData() {
+        return "data from MySqlDatabase";
+    }
+}
